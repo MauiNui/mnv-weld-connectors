336eb851c5096c441052bd3c1b33f2bdf193f05c
BL <141443155+MauiNuiVenison@users.noreply.github.com>
2023-11-01 06:03:37 -1000

    Update app.js

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ app.get('/recharge/subscriptions/schema', (req, res) => {
 
 // Starting the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on ${PORT}`);
 });
